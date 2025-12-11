b0656dcec017a114438ec71b5a8153aa1837201e
moy09-code <moyo2k24@gmail.com>
2025-12-11 08:26:53 +0100

    working build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import {Home} from './pages/home';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <Routes>
+           <Route path='/' element={Home} /> 
+        </Routes>   
+    </BrowserRouter>
 )
